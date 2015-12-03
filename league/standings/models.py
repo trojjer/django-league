54e068574801bb1616f5d84c74983ebe4cffe53d
Marc Kirkwood <marc.kirkwood@sslpost.com>
2015-12-03 12:46:30 +0000

    New TeamStanding model and proper first migration. Plugged model into StandingListView.

--- league/standings/models.py
+++ league/standings/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class TeamStanding(models.Model):
+    """Represents a team's standing.
+    """
+    against = models.IntegerField()
+    drawn = models.IntegerField()
+    goals_for = models.IntegerField()
+    lost = models.IntegerField()
+    played = models.IntegerField()
+    points = models.IntegerField()
+    start_day_position = models.IntegerField()
+    won = models.IntegerField()
+    away_against = models.IntegerField()
+    away_drawn = models.IntegerField()
+    away_for = models.IntegerField()
+    away_lost = models.IntegerField()
+    away_played = models.IntegerField()
+    away_points = models.IntegerField()
+    away_position = models.IntegerField()
+    away_won = models.IntegerField()
+    home_against = models.IntegerField()
+    home_drawn = models.IntegerField()
+    home_for = models.IntegerField()
+    home_lost = models.IntegerField()
+    home_played = models.IntegerField()
+    home_points = models.IntegerField()
+    home_position = models.IntegerField()
+    home_won = models.IntegerField()
--- league/standings/views.py
+++ league/standings/views.py
@@ -1,7 +1,9 @@
 from django.views.generic.list import ListView
 
+from .models import TeamStanding
+
 
 class StandingsListView(ListView):
     """List the teams and their standings.
     """
-    pass
+    model = TeamStanding
